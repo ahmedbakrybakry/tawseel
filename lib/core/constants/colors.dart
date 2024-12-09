0eb7199e7e482e8510aea977e5f275eabe93c9f9
AHMED <ab8327243@gmail.com>
2024-12-09 20:44:01 +0200

    added assets

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primary = Colors.blue;
+  static const Color secondary = Color(0xFF000000);
+  static const Color white = Colors.white;
+  static const Color grey = Colors.grey;
+}
---
+++ lib/core/constants/images.dart
@@ -0,0 +1,8 @@
+class Images{
+  static const String logo = 'assets/images/logo.png';
+  static const String correct = 'assets/images/correct.jpg';
+  static const String direction = 'assets/images/direction.png';
+  static const String onboarding = 'assets/images/onboarding_background.png';
+  static const String welcome = 'assets/images/welcome-bro.svg';
+
+}
